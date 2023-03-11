e94d570647816162e8aca789d2a4fc2d96c20ed8
Dawson Cohen <dawson@the-cohens.com>
2023-03-11 15:35:41 -0500

    fixed character repalce
    fixes character replace

--- curouter/router.js
+++ curouter/router.js
@@ -1,5 +1,5 @@
 if(!hostname.includes("ezproxy.cul.columbia.edu")) {
-  let new_url = new URL("https://"+hostname.replace(".","-")+".ezproxy.cul.columbia.edu");
+  let new_url = new URL("https://"+ hostname.split('.').join('-') +".ezproxy.cul.columbia.edu");
   new_url.pathname = old_url.pathname;
   new_url.search = old_url.search;
 
